3311b1a175ae2eac10e584069b0e443afa0d3a8f
VelizAndres <dandresveliz@gmail.com>
2020-09-04 23:53:58 -0600

    Creacion interna de Nodo

--- Laboratorio1-ED2/Nodo.cs
+++ Laboratorio1-ED2/Nodo.cs
@@ -2,7 +2,15 @@
 
 namespace Laboratorio1_ED2
 {
-    public class Nodo
+    public class Nodo<T>
     {
+        private Nodo<T>[] padres;
+        private Nodo<T>[] hijos;
+        private T[] valores;
+
+        public T[] Valores { get => valores; set => valores = value; }
+        internal Nodo<T>[] Padres { get => padres; set => padres = value; }
+        internal Nodo<T>[] Hijos { get => hijos; set => hijos = value; }
     }
+
 }
