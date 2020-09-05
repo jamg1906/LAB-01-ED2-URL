ef1da5bf69d2f8cfb16f6c5e081926b09ba3cb61
VelizAndres <dandresveliz@gmail.com>
2020-09-05 00:11:31 -0600

    Create Class MultiWayTree

---
+++ Laboratorio1-ED2/MultiwayTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Laboratorio1_ED2;
+
+namespace Laboratorio1_ED2
+{
+    class MultiwayTree<T>
+    {
+        //Nodo raiz
+        Nodo<T> root;
+
+        public void 
+
+
+    }
+}
