cd3c907620f6ef4e4c1fc07d2970f5bde72f16c8
Javier Morales <jamg1906@gmail.com>
2020-09-08 15:21:51 -0600

    MWTree Insertion implemented

--- Laboratorio1-ED2/Structures/MultiwayTree.cs
+++ Laboratorio1-ED2/Structures/MultiwayTree.cs
@@ -7,18 +7,56 @@ namespace Laboratorio1_ED2
 {
     class MultiwayTree<T> where T: IComparable
     {
-        //Nodo raiz
         Node<T> root { get; set; }
         public static int m = 2;
-        public static int Count;
+        public static int Count = 0;
         
         public void InsertMultiWay(T value)
         {
             if (root == null)
             {
-                Node<T> nodeF = new Node<T>(value);
-
+                root = new Node<T>(value, m);
             }
+            else
+            {
+                Node<T> temp = root;
+                int[] ParentsIndex = new int[2];
+                Node<T> temp2 = temp;
+                while (temp.capacityLeft == 0)
+                {
+                    bool found = false;
+                    for (int i = 0; i < (m - 1); i++)
+                    {
+                        temp2 = temp;
+                        if (value.CompareTo(temp.Valores[i]) == -1 && !found)
+                        {
+                            found = true;
+                            temp = temp.Children[i];
+                            ParentsIndex[0] = i;
+                            ParentsIndex[1] = i++;
+
+                        }
+                        else if (i == ((m - 1) - 1) && !found)
+                        {
+                            temp = temp.Children[m - 1];
+                            ParentsIndex[0] = i;
+                            ParentsIndex[1] = i++;
+                        }
+                    }
+                }
+                if (temp == null)
+                {
+                    temp = new Node<T>(value, m);
+                    temp.ParentNode = temp2;
+                    temp.Parents[0] = temp.ParentNode.Valores[ParentsIndex[0]];
+                    temp.Parents[1] = temp.ParentNode.Valores[ParentsIndex[1]];
+                }
+                else
+                {
+                    temp.InsertInNode(value);
+                }
+            }
+            Count++;
         }
          
 
--- Laboratorio1-ED2/Structures/Node.cs
+++ Laboratorio1-ED2/Structures/Node.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Laboratorio1_ED2
 {
@@ -7,14 +8,47 @@ namespace Laboratorio1_ED2
         public static int m;
         public T Value { get; set; }
         public T[] Valores = new T[m - 1];
-        private Node<T>[] Parents = new Node<T>[2];
-        private Node<T>[] Children = new Node<T>[m];
+        public int capacityLeft = m;
+        public T[] Parents = new T[2];
+        public Node<T> ParentNode;
+        public Node<T>[] Children = new Node<T>[m];
+        public Delegate Comparer;
 
 
-        public Node(T value)
+        public Node(T value, int MNode)
         {
-            //aquí habría que hacer la asignación de valores y llamar al método de ordenar
-            Value = value;
+            m = MNode;
+            Valores[0] = value;
+        }
+
+        public void InsertInNode(T value)
+        {
+            for (int i = 0; i < Valores.Length; i++)
+            {
+                if (Valores[i] == null)
+                {
+                    Valores[i] = value;
+                    capacityLeft--;
+                }
+            }
+            SortValuesWithinNode();
+        }
+
+        private void SortValuesWithinNode()
+        {
+            for (int i = 0; i < (Valores.Length-capacityLeft)-1; i++)
+            {
+                for (int j = 0; j < (Valores.Length-capacityLeft) - i -1; j++)
+                {
+                    if ((int)Comparer.DynamicInvoke(Valores[j], Valores[j+1]) == 1)
+                    {
+                        //swap de valores dentro del nodo
+                        T temp = Valores[j];
+                        Valores[j] = Valores[j + 1];
+                        Valores[j + 1] = temp;
+                    }
+                }
+            }
         }
 
     }
