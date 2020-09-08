74204b94c4fd858ae82b0701f1b81ab5033835ec
VelizAndres <dandresveliz@gmail.com>
2020-09-08 15:32:46 -0600

    GET API

---
+++ LAB1-API/Controllers/moviesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LAB1_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class moviesController : ControllerBase
+    {
+
+        // GET api/<moviesController>/inorder
+        [HttpGet("{traversal}")]
+        public ActionResult GetByDate([FromRoute] string traversal)
+        {
+            traversal = traversal.ToLower();
+            var order = 2;
+            if (traversal.Equals("preorder"))
+            {
+                 order = 2 /*Aqui se debe regresar el ordenamiento preorder, a traves del metodo*/;
+            }
+            else if (traversal.Equals("inorder"))
+            {
+                 order = 2 /*Aqui se debe regresar el ordenamiento inorder, a traves del metodo*/;
+            }
+            else if (traversal.Equals("postorder"))
+            {
+                 order = 2 /*Aqui se debe regresar el ordenamiento postorder, a traves del metodo*/;
+            }
+
+            if (order == null) return NotFound();
+            //Se devuelve el texto ordenado
+            return Ok(order);
+        }
+
+
+        [HttpPost]
+        public ActionResult Post([FromBody] string Tipo)
+        {
+            int cantidadhijos = int.Parse(Tipo);
+
+            
+            return BadRequest();
+        }
+
+
+
+        // GET: api/<moviesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<moviesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<moviesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<moviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<moviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LAB1-API/Models/mMovies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB1_API.Models
+{
+    public class mMovies
+    {
+
+        private string director;
+        private double imdbRating;
+        private string genre;
+        private DateTime releaseDate;
+        private int rottenTomatoesRating;
+        private string title;
+
+        public string Director { get => director; set => director = value; }
+        public double ImdbRating { get => imdbRating; set => imdbRating = value; }
+        public string Genre { get => genre; set => genre = value; }
+        public DateTime ReleaseDate { get => releaseDate; set => releaseDate = value; }
+        public int RottenTomatoesRating { get => rottenTomatoesRating; set => rottenTomatoesRating = value; }
+        public string Title { get => title; set => title = value; }
+    }
+}
