b06703e040efe67562bb7d973382fae3c40cd867
jamg1906 <jamg1906@gmail.com>
2020-09-11 14:58:05 -0600

    ConsoleApp completed

--- Laboratorio1-ED2-ConsoleApp/Program.cs
+++ Laboratorio1-ED2-ConsoleApp/Program.cs
@@ -28,6 +28,13 @@ namespace Laboratorio1_ED2_ConsoleApp
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (t.Length / 2)) + "}", t) + "\n");
             Console.ResetColor();
         }
+        public static void TitleOption2()
+        {
+            string t = "--INGRESO MANUAL DE NÚMEROS--";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (t.Length / 2)) + "}", t) + "\n");
+            Console.ResetColor();
+        }
         public static void TitleTraversal(int n)
         {
             switch (n)
@@ -165,13 +172,17 @@ namespace Laboratorio1_ED2_ConsoleApp
                                             }
                                             iterardor++;
                                         }
-                                        Console.WriteLine(result);
-                                        Console.WriteLine("Finalizaron los recorridos. Presione una tecla para volver al menú principal.");
+                                        Console.WriteLine(result + "\n");
+                                        string f = "Finalizaron los recorridos. Presione una tecla para volver al menú principal.";
+                                        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (f.Length / 2)) + "}", f) + "\n");
                                         Console.ReadKey();
                                     }
                                     catch
                                     {
-                                        Console.WriteLine("Ocurrió un error. Se regresará al menú principal.");
+                                        Header();
+                                        string e = "Ocurrió un error. Presione una tecla para volver al menú principal.";
+                                        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (e.Length / 2)) + "}", e) + "\n");
+                                        Console.ReadKey();
                                         a = false;
                                     }
                                 }
@@ -179,25 +190,105 @@ namespace Laboratorio1_ED2_ConsoleApp
                             break;
                         case 2:
                             {
-                                bool a = true;
-                                while (a)
+                                Laboratorio1_ED2.MultiwayTree<int> Tree = new MultiwayTree<int>();
+                                bool b = true;
+                                while (b)
                                 {
                                     try
                                     {
                                         Header();
+                                        TitleOption2();
                                         Console.WriteLine("Ingrese el grado del árbol multicamino (debe ser mayor a 2):");
                                         int m = Convert.ToInt32(Console.ReadLine());
                                         if (m < 2)
                                         {
                                             throw new FormatException();
                                         }
+                                        Tree.SetM(m);
                                         Header();
-
+                                        TitleOption2();
+                                        Console.WriteLine("Ingrese un número para insertarlo en el árbol: (Si desea parar ingrese otro caracter o presione enter)");
+                                        List<int> AddMe = new List<int>();
+                                        bool stop = false;
+                                        while (!stop)
+                                        {
+                                            try
+                                            {
+                                                AddMe.Add(Convert.ToInt32(Console.ReadLine()));
+                                            }
+                                            catch
+                                            {
+                                                stop = true;
+                                                if (AddMe.Count == 0)
+                                                {
+                                                    throw new FormatException();
+                                                }
+                                            }
+                                        }
+                                        Header();
+                                        TitleOption2();
+                                        foreach (int n in AddMe)
+                                        {
+                                            Tree.InsertMultiWay(n);
+                                        }
+                                        Console.WriteLine("Se ha llenado satisfactoriamente el árbol. Presione cualquier tecla para ver los recorridos.");
+                                        Console.ReadKey();
+                                        Header();
+                                        TitleOption2();
+                                        b = false;
+                                        TitleTraversal(1);
+                                        List<int> Recorrido = Tree.Preorder();
+                                        string result = "";
+                                        int iterardor = 1;
+                                        foreach (int number in Recorrido)
+                                        {
+                                            result += number.ToString() + ",";
+                                            if ((iterardor % m) == 0)
+                                            {
+                                                result += Environment.NewLine;
+                                            }
+                                            iterardor++;
+                                        }
+                                        Console.WriteLine(result);
+                                        TitleTraversal(2);
+                                        Recorrido = Tree.Inorder();
+                                        result = "";
+                                        iterardor = 1;
+                                        foreach (int number in Recorrido)
+                                        {
+                                            result += number.ToString() + ",";
+                                            if ((iterardor % m) == 0)
+                                            {
+                                                result += Environment.NewLine;
+                                            }
+                                            iterardor++;
+                                        }
+                                        Console.WriteLine(result);
+                                        TitleTraversal(3);
+                                        Recorrido = Tree.Posorder();
+                                        result = "";
+                                        iterardor = 1;
+                                        foreach (int number in Recorrido)
+                                        {
+                                            result += number.ToString() + ",";
+                                            if ((iterardor % m) == 0)
+                                            {
+                                                result += Environment.NewLine;
+                                            }
+                                            iterardor++;
+                                        }
+                                        Console.WriteLine(result + "\n");
+                                        string f = "Finalizaron los recorridos. Presione una tecla para volver al menú principal.";
+                                        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (f.Length / 2)) + "}", f) + "\n");
+                                        Console.ReadKey();
                                     }
                                     catch
                                     {
-                                        Console.WriteLine("Ocurrió un error. Se regresará al menú principal.");
-                                        a = false;
+                                        Header();
+                                        string e = "Ocurrió un error. Presione una tecla para volver al menú principal.";
+                                        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (e.Length / 2)) + "}", e) + "\n");
+                                        Console.ReadKey();
+                                        b = false;
                                     }
                                 }
                             }
